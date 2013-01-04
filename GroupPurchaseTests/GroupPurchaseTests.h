e10ad62ca9bc0ec4f923e46ad18fc3951813706d
xcode <lt@lttekiMacBook-Pro.local>
2013-01-04 23:32:16 +0800

    Initial Commit

---
+++ GroupPurchaseTests/GroupPurchaseTests.h
@@ -0,0 +1,13 @@
+//
+//  GroupPurchaseTests.h
+//  GroupPurchaseTests
+//
+//  Created by xcode on 13-1-4.
+//  Copyright (c) 2013年 LiHong. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface GroupPurchaseTests : SenTestCase
+
+@end
---
+++ GroupPurchaseTests/GroupPurchaseTests.m
@@ -0,0 +1,32 @@
+//
+//  GroupPurchaseTests.m
+//  GroupPurchaseTests
+//
+//  Created by xcode on 13-1-4.
+//  Copyright (c) 2013年 LiHong. All rights reserved.
+//
+
+#import "GroupPurchaseTests.h"
+
+@implementation GroupPurchaseTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in GroupPurchaseTests");
+}
+
+@end
