7295be59809b599346f098e0a40549f9f431bd2e
xcode <lt@lttekiMacBook-Pro.local>
2013-01-08 15:34:17 +0800

    定义了部分和服务器交互的api，添加了FMDB库,删除了原来使用核心数据的代码。

--- GroupPurchase/AppDelegate.h
+++ GroupPurchase/AppDelegate.h
@@ -12,18 +12,12 @@
 
 @property (strong, nonatomic) UIWindow *window;
 
-@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
-@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
-@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
-
-- (void)saveContext;
-- (NSURL *)applicationDocumentsDirectory;
-
 /* 用于检测是否能连接到服务器.
  * -(BOOL)isReachable;        // 能连接到网络?
  * -(BOOL)isReachableViaWWAN; // 通过3G连接?
  * -(BOOL)isReachableViaWiFi; // 通过WiFi连接?
  * 参考:http://stackoverflow.com/questions/7938650/ios-detect-3g-or-wifi */
+
 @property(readonly, strong, nonatomic) Reachability *reach;
 
 @end
---
+++ GroupPurchase/ClientToServerAPI.m
@@ -0,0 +1,16 @@
+//
+//  ClientToServerAPI.m
+//  GroupPurchase
+//
+//  Created by xcode on 13-1-8.
+//  Copyright (c) 2013年 LiHong. All rights reserved.
+//
+
+#import "ClientToServerAPI.h"
+
+@implementation ClientToServerAPI
+
+- (NSArray *)getGoodsList:(GoodsType)type pageSize:(NSUInteger)size offsetIndex:(NSUInteger)index{return nil;}
+- (NSArray *)getNearGoodsList:(CLLocationCoordinate2D)coordinate{return nil;}
+
+@end
